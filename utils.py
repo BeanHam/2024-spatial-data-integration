240255e27a986f913885d6e9bf2b97c3a8e63497
Bin Han <39442036+BeanHam@users.noreply.github.com>
2024-11-18 15:52:35 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,12 +18,14 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 MODEL_SUFFIXES = {
     'openai': '',
     'mistral': '</s>',
-    'llama3': '</s>'
+    'llama3': '</s>',
+    'qwen': '<|im_end|>'
 }
 
 MODEL_PATHS = {
     'mistral': 'beanham/spatial_join_mistral',
     'llama3': 'beanham/spatial_join_llama3',
+    'qwen':,
     'gpt-4o-mini-2024-07-18': 'ft:gpt-4o-mini-2024-07-18:uw-howe-lab::AUloL5Yu'
 }
 
