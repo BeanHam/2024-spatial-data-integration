e1fd7d8a2f2de4f17ab5072fb4e9680a6042fd9f
Bin Han <39442036+BeanHam@users.noreply.github.com>
2024-11-26 14:51:55 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 pip install -r requirements.txt
-#pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
-#pip install unsloth
-#pip uninstall unsloth -y && pip install --upgrade --no-cache-dir --no-deps git+https://github.com/unslothai/unsloth.git
+
+%%capture
+pip install unsloth
+pip uninstall unsloth -y && pip install --upgrade --no-cache-dir --no-deps git+https://github.com/unslothai/unsloth.git
