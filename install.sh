d972264dded2a958287d4881c53e633c567d137a
BeanHam <binhan96816@gmail.com>
2024-11-26 22:53:56 +0000

    requirements all met

--- install.sh
+++
@@ -1,5 +0,0 @@
-pip install -r requirements.txt
-
-%%capture
-pip install unsloth
-pip uninstall unsloth -y && pip install --upgrade --no-cache-dir --no-deps git+https://github.com/unslothai/unsloth.git
