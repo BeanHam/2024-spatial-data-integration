14a087f2c4be1f26a4a4770a830b834f3eaa9eb8
Bin Han <39442036+BeanHam@users.noreply.github.com>
2024-12-01 14:57:14 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,14 +18,3 @@ MODEL_REPOS = {
     'qwen': 'Qwen/Qwen2.5-7B-Instruct',
     'gpt-4o-mini-2024-07-18': 'ft:gpt-4o-mini-2024-07-18:uw-howe-lab::AUloL5Yu'
 }
-
-
-
-def process_train_data(train, metric_name, metric_value):
-    
-    if metric_name == 'degree':
-        positive = train.filter(lambda x: ((x['label']==1) & (x['min_angle']<=metric_value)) )
-        negative = train.filter(lambda x: ((x['label']==0) & (x['min_angle']>metric_value)) )
-        new_train=concatenate_datasets([positive,negative]).shuffle()
-        
-    return new_train
