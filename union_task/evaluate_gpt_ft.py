9005190ca249f8b5740da20c79a5a41cf65944b6
Bin Han <39442036+BeanHam@users.noreply.github.com>
2025-02-12 12:09:52 -0800

    Update evaluate_gpt_ft.py

--- union_task/evaluate_gpt_ft.py
+++ union_task/evaluate_gpt_ft.py
@@ -12,7 +12,7 @@ def evaluate_gpt(data, client, model):
     model_outputs = []            
     for i in tqdm(range(len(data))):
         
-        user_input="Sidewalk: "+str(data['sidewalk'][i])+"\nRoad: "+str(data['road'][i])
+        user_input="Sidewalk 1: "+str(data['sidewalk'][i])+"\nSidewalk 2: "+str(data['road'][i])
         response = client.chat.completions.create(
             model=model,
             messages=[
@@ -36,7 +36,7 @@ def main():
     #-------------------    
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_id', type=str, default='4o_mini')
-    parser.add_argument('--dataset', type=str, default='beanham/spatial_join_dataset')
+    parser.add_argument('--dataset', type=str, default='beanham/spatial_union_dataset')
     parser.add_argument('--key', type=str, default='openaikey')
     parser.add_argument('--metric_name', type=str, default='degree')
     args = parser.parse_args()
@@ -44,12 +44,10 @@ def main():
     args.save_path=f'inference_results/{args.metric_name}/'
     if not path.exists(args.save_path):
         makedirs(args.save_path)
-
     if args.metric_name == 'degree':
-        #args.metric_values = [1,2,5,10,20]
-        args.metric_values = [20]
-    elif args.metric_name == 'distance':
         args.metric_values = [1,2,3,4,5]
+    elif args.metric_name == 'area':
+        args.metric_values = [0.5,0.6,0.7,0.8,0.9]
         
     # ----------------------
     # Load Data
