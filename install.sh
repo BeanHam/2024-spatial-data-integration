db81c4196892d30d5d8c5aab90f52edcac63ee2d
Bin Han <39442036+BeanHam@users.noreply.github.com>
2025-02-18 15:26:41 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 pip install -r requirements_new.txt
-pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
-
-#pip install --no-deps bitsandbytes accelerate xformers==0.0.29 peft trl triton
-#pip install --no-deps cut_cross_entropy unsloth_zoo
-#pip install sentencepiece protobuf datasets huggingface_hub hf_transfer
-#pip install --no-deps unsloth
+pip install --no-deps cut_cross_entropy tyro unsloth_zoo
+pip install protobuf hf_transfer
+pip install --no-deps unsloth
 
 #pip install -r requirements.txt
 #pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
