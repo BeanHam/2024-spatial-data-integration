9425020e6a726a4e1eeff5a16d46692272d8a4c6
Bin Han <39442036+BeanHam@users.noreply.github.com>
2025-02-18 13:44:51 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-pip install -r requirements.txt
+pip install -r requirements_new.txt
 pip install --no-deps bitsandbytes accelerate xformers==0.0.29 peft trl triton
 pip install --no-deps cut_cross_entropy unsloth_zoo
 pip install sentencepiece protobuf datasets huggingface_hub hf_transfer
 pip install --no-deps unsloth
 
+#pip install -r requirements.txt
 #pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
 #pip install --no-deps trl peft accelerate bitsandbytes
