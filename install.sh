bc63cb39bdc52d99010168c26260120245a7f4a5
Bin Han <39442036+BeanHam@users.noreply.github.com>
2025-02-18 17:06:25 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 conda create --name spatial_integration python=3.11 pytorch-cuda=12.1 pytorch cudatoolkit xformers -c pytorch -c nvidia -c xformers -y
+conda initiate
 conda activate unsloth_env
 pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
 pip install --no-deps trl peft accelerate bitsandbytes
