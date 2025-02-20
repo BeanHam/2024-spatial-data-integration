42628f3e5c81689b53f999fab5fdc209226964f5
Bin Han <39442036+BeanHam@users.noreply.github.com>
2025-02-19 16:07:07 -0800

    Update evaluate_open_model_base.py

--- join_task/evaluate_open_model_base.py
+++ join_task/evaluate_open_model_base.py
@@ -15,7 +15,7 @@ def evaluate(model, tokenizer, data):
     for text in tqdm(data['text']):
         start_decode = len(tokenizer.encode(text, truncation=True, max_length=2048))        
         inputs = tokenizer(text, return_tensors = "pt", max_length=2048).to("cuda")
-        response = model.generate(**inputs, max_new_tokens = 10, temperature=0)
+        response = model.generate(**inputs, max_new_tokens = 10)
         response = tokenizer.decode(response[0][start_decode:])
         outputs.append(response)
     return outputs
