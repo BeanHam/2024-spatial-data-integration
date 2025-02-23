b434128d35957ee8c7b74072e5df70c81d6a4356
Bin Han <39442036+BeanHam@users.noreply.github.com>
2025-02-23 13:45:58 -0800

    Update utils.py

--- join_task/utils.py
+++ join_task/utils.py
@@ -53,6 +53,7 @@ MODEL_PATHS = {
 
 MODEL_PATHS_WEAK_LABELS = {
     'llama3_degree_1_weak': 'beanham/spatial_join_llama3_degree_1_weak',
+    'llama3_degree_10_weak': 'beanham/spatial_join_llama3_degree_10_weak',
     'llama3_distance_1_weak': 'beanham/spatial_join_llama3_distance_1_weak',
     '4o_mini_degree_1_weak': 'ft:gpt-4o-mini-2024-07-18:uw-howe-lab::B39ZnKI2',
 }
