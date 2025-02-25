732442b7c56b54c47480edfef15d2f7959524e81
BeanHam <binhan96816@gmail.com>
2025-02-25 15:46:12 -0800

    added gpt vision base code

---
+++ join_task/evaluate_gpt_base_vision.py
@@ -0,0 +1,123 @@
+import os
+import json
+import base64
+import argparse
+import requests
+import numpy as np
+
+from utils import *
+from PIL import Image
+from tqdm import tqdm
+from openai import OpenAI
+from visual_prompts import *
+from os import path, makedirs
+from itertools import product
+from datasets import load_dataset
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+def write_completion_request(text, base64_image, model):
+    """
+    Compose completion request.
+    """    
+    completion = {
+        "model": model,
+        "messages": [
+            {"role": "user",
+             "content": [
+                 {"type": "text", "text": text},
+                 {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}}
+             ]}
+        ],
+        "temperature": 0,
+        "max_tokens": 10,
+        "top_p":1
+    }
+    return completion
+    
+def evaluate_gpt_4o_vision(data, model, index, path, api_web, headers):
+    model_outputs = []
+    for i in tqdm(range(len(data))):
+        img_path=path+f'{index[i]}.png'
+        base64_image = encode_image(img_path)
+        completion = write_completion_request(data['text'][i], base64_image, model)
+        response = requests.post(api_web, headers=headers, json=completion)
+        model_outputs.append(response.json()['choices'][0]['message']['content'])
+    return model_outputs
+
+#-----------------------
+# Main Function
+#-----------------------
+def main():
+    
+    #-------------------
+    # parameters
+    #-------------------    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_id', type=str, default='4o_mini')
+    parser.add_argument('--dataset', type=str, default='beanham/spatial_join_dataset')
+    parser.add_argument('--key', type=str, default='openaikey')
+    args = parser.parse_args()
+    args.save_path=f'inference_results/base/{args.model_id}_visual/'
+    if not path.exists(args.save_path):
+        makedirs(args.save_path)
+        
+    args.model_repo = MODEL_REPOS[args.model_id]
+    data = load_dataset(args.dataset)
+    path='../../2024-spatial-data-integration-exp/join_imgs/small_imgs/'
+    api_web = "https://api.openai.com/v1/chat/completions"
+    headers = {"Content-Type": "application/json","Authorization": f"Bearer {args.key}"}
+
+    ## load configs
+    methods = ['zero_shot', 'few_shot']    
+    modes = ['no_heur', 'with_heur_hint', 'with_heur_value']
+    heuristics = ['angle', 'distance', 'comb']
+    configs=['_'.join(i) for i in list(product(methods, modes, heuristics))]
+    configs.remove('zero_shot_no_heur_angle')
+    configs.remove('zero_shot_no_heur_distance')
+    configs.remove('zero_shot_no_heur_comb')
+    configs.remove('few_shot_no_heur_angle')
+    configs.remove('few_shot_no_heur_distance')
+    configs.remove('few_shot_no_heur_comb')
+    configs.append('zero_shot_no_heur')
+    configs.append('few_shot_no_heur')
+    configs=['zero_shot_no_heur']
+    
+    ## load test set index
+    index=[]
+    with open('../../2024-spatial-data-integration-exp/join_index/test.txt', 'r') as file:
+        for line in file:
+            index.append(line.strip())
+    
+    #-----------------------------
+    # loop through parameters
+    #-----------------------------
+    for config in configs:
+        print('=================================')
+        print(f'Config: {config}...')
+        def formatting_prompts_func(example):
+            output = ""
+            if 'value_angle' in config:
+                input = "Sidewalk: "+str(example['sidewalk'])+"\nRoad: "+str(example['road'])+\
+                        "\nmin_angle: "+str(example['min_angle'])
+            elif 'value_distance' in config:
+                input = "Sidewalk: "+str(example['sidewalk'])+"\nRoad: "+str(example['road'])+\
+                        "\nmin_distance: "+str(example['min_euc_dist'])    
+            elif 'value_comb' in config:
+                input = "Sidewalk: "+str(example['sidewalk'])+"\nRoad: "+str(example['road'])+\
+                        "\nmin_angle: "+str(example['min_angle'])+"\nmin_distance: "+str(example['min_euc_dist'])        
+            else:
+                input = "Sidewalk: "+str(example['sidewalk'])+"\nRoad: "+str(example['road'])
+            text = base_alpaca_prompt.format(base_instruction, input, output)
+            return { "text" : text}
+
+        base_instruction=INSTRUCTIONS[config]
+        test = data['test'].map(formatting_prompts_func)
+        args.save_name = f"{args.model_id}_{config}.npy"        
+        outputs = evaluate_gpt_4o_vision(test, index, args.model_repo, path, api_web, headers)
+        np.save(args.save_path+args.save_name, outputs)
+        
+if __name__ == "__main__":
+    main()
---
+++ join_task/visual_prompts.py
@@ -0,0 +1,251 @@
+## =================
+## base evaluation
+## =================
+base_alpaca_prompt = """### Instruction:
+{}
+
+### Input:
+{}
+
+### Response: 
+{}"""
+
+## ===============================
+## base evaluation -- instructions
+## ===============================
+
+
+## -------------------
+## no heuristics
+## -------------------
+instruction_no_heur="""You are a helpful geospatial analysis assistant. I will provide you with a pair of (sidewalk, road) geometries in GeoJSON format, along with a satellite image that visualizes their orientations. In the image, the blue line represents the road, and the red line represents the sidewalk. Your task is to determine whether the sidewalk runs alongside the road. If it does, return 1. Otherwise, return 0. No explaination is needed."""
+
+## -------------------
+## with heuristic hint
+## -------------------
+
+## angle
+instruction_heur_hint_angle="""You are a helpful geospatial analysis assistant. I will provide you with a pair of (sidewalk, road) geometries in GeoJSON format, along with a satellite image that visualizes their orientations. In the image, the blue line represents the road, and the red line represents the sidewalk. Your task is to determine whether the sidewalk runs alongside the road by evaluating the following condition:
+
+- Parallelism: The sidewalk should be approximately parallel to the road, with only a small angle difference between their orientations.
+
+If the condition is satisfied, return 1. Otherwise, return 0. No explaination is needed. """
+
+## distance
+instruction_heur_hint_distance="""You are a helpful geospatial analysis assistant. I will provide you with a pair of (sidewalk, road) geometries in GeoJSON format, along with a satellite image that visualizes their orientations. In the image, the blue line represents the road, and the red line represents the sidewalk. Your task is to determine whether the sidewalk runs alongside the road by evaluating the following condition:
+
+- Clearance: The sidewalk and road must not overlap or intersect, and they must maintain a certain distance apart. 
+
+If the condition is satisfied, return 1. Otherwise, return 0. No explaination is needed. """
+
+## combination
+instruction_heur_hint_comb="""You are a helpful geospatial analysis assistant. I will provide you with a pair of (sidewalk, road) geometries in GeoJSON format, along with a satellite image that visualizes their orientations. In the image, the blue line represents the road, and the red line represents the sidewalk. Your task is to determine whether the sidewalk runs alongside the road by evaluating the following conditions:
+
+- Parallelism: The sidewalk should be approximately parallel to the road, with only a small angle difference between their orientations.
+- Clearance: The sidewalk and road must not overlap or intersect, and they must maintain a certain distance apart. 
+
+If both conditions are satisfied, return 1. Otherwise, return 0. No explaination is needed. """
+
+## -------------------
+## with heuristic value
+## -------------------
+
+## angle
+instruction_heur_value_angle="""You are a helpful geospatial analysis assistant. I will provide you with a pair of (sidewalk, road) geometries in GeoJSON format, along with a key statistics:
+
+- min_angle: The minimum angle (in degrees) between the sidewalk and the road.
+
+I will also provide a satellite image that visualizes their orientations. In the image, the blue line represents the road, and the red line represents the sidewalk. Your task is to determine whether the sidewalk runs alongside the road by evaluating the following condition:
+
+- Parallelism: The sidewalk should be approximately parallel to the road, with only a small angle difference between their orientations. The min_angle value provides a measure of this alignment.
+
+If the condition is satisfied, return 1. Otherwise, return 0. No explaination is needed."""
+
+## distance
+instruction_heur_value_distance="""You are a helpful geospatial analysis assistant. I will provide you with a pair of (sidewalk, road) geometries in GeoJSON format, along with a key statistics:
+
+- min_distance: The minimum distance (in meters) between the sidewalk and the road.
+
+I will also provide a satellite image that visualizes their orientations. In the image, the blue line represents the road, and the red line represents the sidewalk. Your task is to determine whether the sidewalk runs alongside the road by evaluating the following condition:
+
+- Clearance: The sidewalk and road must not overlap or intersect, and they must maintain a certain distance apart. The min_distance value helps quantify this proximity.
+
+If the condition is satisfied, return 1. Otherwise, return 0. No explaination is needed."""
+
+## combination
+instruction_heur_value_comb="""You are a helpful geospatial analysis assistant. I will provide you with a pair of (sidewalk, road) geometries in GeoJSON format, along with two key statistics:
+
+- min_angle: The minimum angle (in degrees) between the sidewalk and the road.
+- min_distance: The minimum distance (in meters) between the sidewalk and the road.
+
+I will also provide a satellite image that visualizes their orientations. In the image, the blue line represents the road, and the red line represents the sidewalk. Your task is to determine whether the sidewalk runs alongside the road by evaluating the following conditions:
+
+- Parallelism: The sidewalk should be approximately parallel to the road, with only a small angle difference between their orientations. The min_angle value provides a measure of this alignment.
+- Clearance: The sidewalk and road must not overlap or intersect, and they must maintain a certain distance apart. The min_distance value helps quantify this proximity.
+
+If both conditions are satisfied, return 1. Otherwise, return 0. No explaination is needed."""
+
+
+## ===============================
+## base evaluation -- examples
+## ===============================
+
+## -------------------
+## no heuristics
+## -------------------
+examples_no_heur="""
+
+### First Exmaple:
+Sidewalk: {'coordinates': [[-122.15646960000001, 47.58741259999999], [-122.1562564, 47.58744089999999]], 'type': 'LineString'}
+Road: {'coordinates': [[-122.1563888, 47.5874271], [-122.1563897, 47.5874341], [-122.1564949, 47.5890663], [-122.1564975, 47.5890982]], 'type': 'LineString'}
+Response: 0
+
+### Second Exmaple:
+Sidewalk: {'coordinates': [[-122.13341579999998, 47.54698270000001], [-122.1334011, 47.5468383]], 'type': 'LineString'}
+Road: {'coordinates': [[-122.1328993, 47.5458957], [-122.1329478, 47.5460104], [-122.1330183, 47.5461317], [-122.1330885, 47.5462402], [-122.1333795, 47.5466214], [-122.1334411, 47.5467369], [-122.1334757, 47.5468199], [-122.1335148, 47.5469582]], 'type': 'LineString'}
+Response: 1"""
+
+## -------------------
+## with heuristic hint
+## -------------------
+examples_heur_hint="""
+
+### First Exmaple:
+Sidewalk: {'coordinates': [[-122.15646960000001, 47.58741259999999], [-122.1562564, 47.58744089999999]], 'type': 'LineString'}
+Road: {'coordinates': [[-122.1563888, 47.5874271], [-122.1563897, 47.5874341], [-122.1564949, 47.5890663], [-122.1564975, 47.5890982]], 'type': 'LineString'}
+Response: 0
+
+### Second Exmaple:
+Sidewalk: {'coordinates': [[-122.13341579999998, 47.54698270000001], [-122.1334011, 47.5468383]], 'type': 'LineString'}
+Road: {'coordinates': [[-122.1328993, 47.5458957], [-122.1329478, 47.5460104], [-122.1330183, 47.5461317], [-122.1330885, 47.5462402], [-122.1333795, 47.5466214], [-122.1334411, 47.5467369], [-122.1334757, 47.5468199], [-122.1335148, 47.5469582]], 'type': 'LineString'}
+Response: 1"""
+
+
+## -------------------
+## with heuristic value
+## -------------------
+## angle
+examples_heur_value_angle="""
+
+### First Exmaple:
+Sidewalk: {'coordinates': [[-122.15646960000001, 47.58741259999999], [-122.1562564, 47.58744089999999]], 'type': 'LineString'}
+Road: {'coordinates': [[-122.1563888, 47.5874271], [-122.1563897, 47.5874341], [-122.1564949, 47.5890663], [-122.1564975, 47.5890982]], 'type': 'LineString'}
+min_angle: 86.12658269425465
+Response: 0
+
+### Second Exmaple:
+Sidewalk: {'coordinates': [[-122.13341579999998, 47.54698270000001], [-122.1334011, 47.5468383]], 'type': 'LineString'}
+Road: {'coordinates': [[-122.1328993, 47.5458957], [-122.1329478, 47.5460104], [-122.1330183, 47.5461317], [-122.1330885, 47.5462402], [-122.1333795, 47.5466214], [-122.1334411, 47.5467369], [-122.1334757, 47.5468199], [-122.1335148, 47.5469582]], 'type': 'LineString'}
+min_angle: 9.973873687169487
+Response: 1"""
+
+## distance
+examples_heur_value_distance="""
+
+### First Exmaple:
+Sidewalk: {'coordinates': [[-122.15646960000001, 47.58741259999999], [-122.1562564, 47.58744089999999]], 'type': 'LineString'}
+Road: {'coordinates': [[-122.1563888, 47.5874271], [-122.1563897, 47.5874341], [-122.1564949, 47.5890663], [-122.1564975, 47.5890982]], 'type': 'LineString'}
+min_distance: 0.6112785794641761
+Response: 0
+
+### Second Exmaple:
+Sidewalk: {'coordinates': [[-122.13341579999998, 47.54698270000001], [-122.1334011, 47.5468383]], 'type': 'LineString'}
+Road: {'coordinates': [[-122.1328993, 47.5458957], [-122.1329478, 47.5460104], [-122.1330183, 47.5461317], [-122.1330885, 47.5462402], [-122.1333795, 47.5466214], [-122.1334411, 47.5467369], [-122.1334757, 47.5468199], [-122.1335148, 47.5469582]], 'type': 'LineString'}
+min_distance: 8.72605420848234
+Response: 1"""
+
+## combination
+examples_heur_value_comb="""
+
+### First Exmaple:
+Sidewalk: {'coordinates': [[-122.15646960000001, 47.58741259999999], [-122.1562564, 47.58744089999999]], 'type': 'LineString'}
+Road: {'coordinates': [[-122.1563888, 47.5874271], [-122.1563897, 47.5874341], [-122.1564949, 47.5890663], [-122.1564975, 47.5890982]], 'type': 'LineString'}
+min_angle: 86.12658269425465
+min_distance: 0.6112785794641761
+Response: 0
+
+### Second Exmaple:
+Sidewalk: {'coordinates': [[-122.13341579999998, 47.54698270000001], [-122.1334011, 47.5468383]], 'type': 'LineString'}
+Road: {'coordinates': [[-122.1328993, 47.5458957], [-122.1329478, 47.5460104], [-122.1330183, 47.5461317], [-122.1330885, 47.5462402], [-122.1333795, 47.5466214], [-122.1334411, 47.5467369], [-122.1334757, 47.5468199], [-122.1335148, 47.5469582]], 'type': 'LineString'}
+min_angle: 9.973873687169487
+min_distance: 8.72605420848234
+Response: 1"""
+
+INSTRUCTIONS = {
+    'zero_shot_no_heur': instruction_no_heur,
+    'zero_shot_with_heur_hint_angle': instruction_heur_hint_angle,
+    'zero_shot_with_heur_hint_distance': instruction_heur_hint_distance,
+    'zero_shot_with_heur_hint_comb': instruction_heur_hint_comb,
+    'zero_shot_with_heur_value_angle': instruction_heur_value_angle,
+    'zero_shot_with_heur_value_distance': instruction_heur_value_distance,
+    'zero_shot_with_heur_value_comb': instruction_heur_value_comb,
+    'few_shot_no_heur': instruction_no_heur+examples_no_heur,
+    'few_shot_with_heur_hint_angle': instruction_heur_hint_angle+examples_heur_hint,
+    'few_shot_with_heur_hint_distance': instruction_heur_hint_distance+examples_heur_hint,
+    'few_shot_with_heur_hint_comb': instruction_heur_hint_comb+examples_heur_hint,
+    'few_shot_with_heur_value_angle': instruction_heur_value_angle+examples_heur_value_angle,
+    'few_shot_with_heur_value_distance': instruction_heur_value_distance+examples_heur_value_distance,
+    'few_shot_with_heur_value_comb': instruction_heur_value_comb+examples_heur_value_comb,
+}
+
+## ============================
+## self correction prompts
+## ============================
+
+base_sc_review_alpaca_prompt = """### Instruction:
+{}
+
+### Input:
+{}
+
+### Response: {}
+
+### Review:
+Please review and identify if there are any problems with the above response."""
+
+base_sc_improve_alpaca_prompt = """### Instruction:
+{}
+
+### Input:
+{}
+
+### Response: {}
+
+### Review:
+Please review and identify if there are any problems with the above response.
+
+{}
+
+### Improve:
+Based on your review, please give your final response, either 0 or 1. 
+
+### Response: """
+
+base_ec_review_alpaca_prompt = """### Instruction:
+{}
+
+### Input:
+{}
+
+### Response: {}
+
+### Review:
+The above response is generated using heuristics. Please carefully review and identify if there are any problems with the above response."""
+
+base_ec_improve_alpaca_prompt = """### Instruction:
+{}
+
+### Input:
+{}
+
+### Response: {}
+
+### Review:
+The above response is generated using heuristics. Please carefully review and identify if there are any problems with the above response.
+
+{}
+
+### Improve:
+Based on your review, please give your final response, either 0 or 1. 
+
+### Response: """
