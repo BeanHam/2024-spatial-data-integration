888f071e9e84bade83ebebaf3988b0be1e9ec4ad
BeanHam <binhan96816@gmail.com>
2025-02-25 17:40:27 -0800

    added deepseek evaluation code

--- join_task/evaluate_deepseek_base.py
+++ join_task/evaluate_deepseek_base.py
@@ -10,7 +10,7 @@ from openai import OpenAI
 from os import path, makedirs
 from datasets import load_dataset
 
-def evaluate_qwen(data, client, model):    
+def evaluate_deepseek(data, client, model):    
     model_outputs = []            
     for i in tqdm(range(len(data))):
         response = client.chat.completions.create(
@@ -20,10 +20,10 @@ def evaluate_qwen(data, client, model):
             ],
             temperature=0,
             max_tokens=10,
-            top_p=1
+            top_p=1,
+            stream=False
         )
         model_outputs.append(response.choices[0].message.content)
-        time.sleep(0.2)
     return model_outputs
 
 #-----------------------
@@ -35,18 +35,18 @@ def main():
     # parameters
     #-------------------    
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_id', type=str, default='qwen')
+    parser.add_argument('--model_id', type=str, default='deepseek')
     parser.add_argument('--dataset', type=str, default='beanham/spatial_join_dataset')
-    parser.add_argument('--key', type=str, default='qwenkey')
+    parser.add_argument('--key', type=str, default='deepseekkey')
     args = parser.parse_args()
     args.save_path=f'inference_results/base/{args.model_id}/'
     if not path.exists(args.save_path):
         makedirs(args.save_path)
         
     args.model_repo = MODEL_REPOS[args.model_id]
-    client = OpenAI(api_key=args.key,
-                    base_url="https://dashscope-intl.aliyuncs.com/compatible-mode/v1")
+    client = OpenAI(api_key=args.key,base_url="https://api.deepseek.com")
     data = load_dataset(args.dataset)
+    
     methods = ['zero_shot', 'few_shot']    
     modes = ['no_heur', 'with_heur_hint', 'with_heur_value']
     heuristics = ['angle', 'distance', 'comb']
@@ -83,9 +83,9 @@ def main():
             return { "text" : text}
             
         base_instruction=INSTRUCTIONS[config]
-        test = data['test'].map(formatting_prompts_func)
+        test = data['test'].select(range(10)).map(formatting_prompts_func)
         args.save_name = f"{args.model_id}_{config}.npy"
-        outputs = evaluate_qwen(test, client, args.model_repo)
+        outputs = evaluate_deepseek(test, client, args.model_repo)
         np.save(args.save_path+args.save_name, outputs)
         
 if __name__ == "__main__":
