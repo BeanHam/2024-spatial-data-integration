628de94157857dc89bc5dfa4690a042159c7f598
BeanHam <binhan96816@gmail.com>
2025-02-26 17:12:44 -0800

    cleaned code

--- join_task/evaluate_base_ec_qwen_fs_comb.py
+++ join_task/evaluate_base_ec_qwen_fs_comb.py
@@ -63,9 +63,9 @@ def main():
         args.metric_values = [1,2,5,10,20]
 
     data = load_dataset(args.dataset)
-    configs=['zero_shot_with_heur_value_angle',
-             'zero_shot_with_heur_value_comb',
-             'few_shot_with_heur_value_angle',
+    configs=[#'zero_shot_with_heur_value_angle',
+             #'zero_shot_with_heur_value_comb',
+             #'few_shot_with_heur_value_angle',
              'few_shot_with_heur_value_comb']    
     args.model_repo = MODEL_REPOS[args.model_id]
     client = OpenAI(api_key=args.key,base_url="https://dashscope-intl.aliyuncs.com/compatible-mode/v1")    
