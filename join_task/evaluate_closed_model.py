d8feb86e02f8bafbe099266d0d6bd32763333e54
Bin Han <39442036+BeanHam@users.noreply.github.com>
2025-03-05 11:13:17 -0800

    Update evaluate_closed_model.py

--- join_task/evaluate_closed_model.py
+++ join_task/evaluate_closed_model.py
@@ -40,9 +40,9 @@ def main():
         makedirs(args.save_path)
         
     args.model_repo = MODEL_REPOS[args.model_id]
-    if args.model=='4o_mini':
+    if args.model_id=='4o_mini':
         client = OpenAI(api_key=args.key)
-    elif args.model=='qwen':
+    elif args.model_id=='qwen':
         client = OpenAI(api_key=args.key,base_url="https://dashscope-intl.aliyuncs.com/compatible-mode/v1")
     data = load_dataset(args.dataset)
     configs=list(INSTRUCTIONS.keys())
